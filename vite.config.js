6255799141ed8924f22e71d2092f40382539a3b0
vallieres13 <felixhbg@gmail.com>
2024-01-20 11:32:27 +0100

    Fix font path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+	base: '/',
 	root: 'src',
 	build: {
 		outDir: '../dist',
@@ -10,7 +11,8 @@ export default defineConfig({
 	plugins: [
 		react({
 			include: '**/*.{jsx,tsx}'
-		})
+		}),
+		splitVendorChunkPlugin()
 	],
 	publicDir: 'static'
 });
