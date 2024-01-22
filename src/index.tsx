f3f10143a8d5b9548e02ca5e057b797ffa620a5f
vallieres13 <felixhbg@gmail.com>
2024-01-22 17:38:29 +0100

    Fix redirect for version component

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('app') as HTMLElement
+	document.getElementById('app') as HTMLElement
 );
 
 root.render(<App />);
